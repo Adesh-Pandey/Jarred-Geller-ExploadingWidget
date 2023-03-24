b24462c43e6a1b437f807e900609d00e92e517fa
Adesh Pandey <adeshpandey836@gmail.com>
2023-03-24 13:23:59 +0545

    animation added for dragging only using native html elements

--- src/Columns.tsx
+++ src/Columns.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect, useRef } from 'react'
 import ColumnComponent from './ColumnComponent';
 import "./column.css"
 import ArrowRightAltIcon from '@mui/icons-material/ArrowRightAlt';
@@ -11,6 +11,7 @@ function Columns() {
     const Base = useSelector((state: RootState) => state.allState.base)
     const [MouseDown, setMouseDown] = useState(false);
     const [Visibility, setVisibility] = useState(true)
+    const containerDiv = useRef<HTMLDivElement>(null);
 
     const ColumnCollection = useSelector((state: RootState) => state.allState.ColumnCollection)
     // const addColumn = () => {
@@ -31,30 +32,23 @@ function Columns() {
         <div className="choose-conversion">
             <div className="borderless-div">
                 <div className="choose-conversion-list">
-                    <select className='choose-conversion-list-option' name="convert-to" id="to">
-                        <option value="1">1</option>
-                        <option value="1">2</option>
-                        <option value="1">3</option>
-                        <option value="1">4</option>
-                        <option value="1">5</option>
-                    </select>
-                    <ArrowRightAltIcon className='reverse-arrow' />
+
                     <select className='choose-conversion-list-option' name="convert-from" id="from">
-                        <option value="2">2</option>
-                        <option value="1">3</option>
-                        <option value="1">4</option>
-                        <option value="1">5</option>
+                        <option value="2"> 1 <ArrowRightAltIcon className='reverse-arrow' /> to 2 </option>
+                        <option value="3">1 <ArrowRightAltIcon className='reverse-arrow' /> to 3</option>
+                        <option value="4">1 <ArrowRightAltIcon className='reverse-arrow' /> to 4</option>
+                        <option value="5">1 <ArrowRightAltIcon className='reverse-arrow' /> to 5</option>
                     </select>
                 </div>
             </div></div>
-        <div className="column-collection-container">
+        <div ref={containerDiv} className="column-collection-container">
 
 
 
             {/* <button onClick={addColumn}>Plus icon</button> */}
             {Visibility &&
                 ColumnCollection.map((elem, idx) => {
-                    return <ColumnComponent order={idx} base={Base} key={idx}
+                    return <ColumnComponent constrainsRef={containerDiv} order={idx} base={Base} key={idx}
                     // MouseDown={MouseDown} setMouseDown={setMouseDown}
                     />
                 })
--- src/redux/mouseSlice.ts
+++ src/redux/mouseSlice.ts
@@ -1,33 +1,44 @@
 import { createSlice } from '@reduxjs/toolkit'
 import type { PayloadAction } from '@reduxjs/toolkit'
 
+export interface MyCircleObj {
+    parentId: number
+}
+
 export interface DotState {
     base: number,
     mouseDown: boolean,
     mouseupLocation: number,
     mouseDownSource: number,
+    mouseDownCircle: number,
     InnerCirclesList: number[],
     ColumnCollection: number[],
+    CircleListObject: MyCircleObj[]
 }
+
 
 const initialState: DotState = {
     base: 2,
     mouseDown: false,
     mouseupLocation: -1,
     mouseDownSource: -1,
-    InnerCirclesList: [0, 0, 0],
-    ColumnCollection: [0, 1, 2]
+    mouseDownCircle: -1,
+    InnerCirclesList: [0, 0, 0, 0, 0],
+    ColumnCollection: [0, 1, 2, 3, 4],
+    CircleListObject: []
 }
 
 export const DotSlice = createSlice({
     name: 'allState',
     initialState,
     reducers: {
-        mouseDownOnTheToken: (state, action: PayloadAction<number>) => {
+        mouseDownOnTheToken: (state, action: PayloadAction<number[]>) => {
             state.mouseDown = true;
-            state.mouseDownSource = action.payload;
+            state.mouseDownSource = action.payload[0];
+            state.mouseDownCircle = action.payload[0]
         },
         mouseUpOnColumn: (state, action: PayloadAction<number>) => {
+            // console.log(action.payload)
             state.mouseDown = false;
             state.mouseupLocation = action.payload;
             let goingToLower = false;
@@ -42,10 +53,27 @@ export const DotSlice = createSlice({
 
             if (!goingToLower) {
                 if (state.InnerCirclesList[state.mouseDownSource] >= numberOfTokenRequired) {
+
+                    state.CircleListObject.reverse()
+                    for (let i = 0; i < numberOfTokenRequired; i++) {
+                        const idx = state.CircleListObject.indexOf({ parentId: state.mouseDownSource })
+                        state.CircleListObject.splice(idx, 1)
+                    }
                     state.InnerCirclesList[state.mouseDownSource] -= numberOfTokenRequired;
                     state.InnerCirclesList[state.mouseupLocation] += 1;
+                    state.CircleListObject.reverse()
+                    state.CircleListObject.push({ parentId: state.mouseupLocation })
                 }
             } else {
+
+                state.CircleListObject.reverse()
+                for (let i = 0; i < numberOfTokenRequired; i++) {
+                    // const idx = state.CircleListObject.indexOf({ parentId: state.mouseupLocation })
+                    state.CircleListObject.push({ parentId: state.mouseupLocation })
+                }
+                state.CircleListObject.reverse()
+                const idx = state.CircleListObject.indexOf({ parentId: state.mouseDownSource })
+                state.CircleListObject.splice(idx, 1)
                 state.InnerCirclesList[state.mouseDownSource] -= 1;
                 state.InnerCirclesList[state.mouseupLocation] += numberOfTokenRequired;
             }
@@ -57,6 +85,7 @@ export const DotSlice = createSlice({
         addTokenInColumn: (state, action: PayloadAction<number>) => {
             state.mouseDown = true;
             state.InnerCirclesList[action.payload] += 1;
+            state.CircleListObject.push({ parentId: action.payload })
         },
         removeTokenInColumn: (state, action: PayloadAction<number>) => {
             state.mouseDown = true;
@@ -64,6 +93,9 @@ export const DotSlice = createSlice({
                 return;
             }
             state.InnerCirclesList[action.payload] -= 1;
+            state.CircleListObject.reverse()
+            state.CircleListObject.splice(state.CircleListObject.indexOf({ parentId: action.payload }), 1)
+            state.CircleListObject.reverse()
         },
 
     },
